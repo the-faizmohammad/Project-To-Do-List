22007c66a2e6c087cca9cf89e62b8fb4c197c4e0
Faiz Mohammad <134736318+the-faizmohammad@users.noreply.github.com>
2023-07-20 03:48:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@
 import './style.css';
 import reloadIcon from './reload.png';
 import moreIcon from './more.png';
+// Select elements from the DOM
+const mainHeading = document.querySelector('.main-heading');
+const addList = document.getElementById('add-list');
