cf6f0921525bcd0a9d5676fc1699c2ccdf813f70
Faiz Mohammad <134736318+the-faizmohammad@users.noreply.github.com>
2023-07-20 03:48:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ import moreIcon from './more.png';
 // Select elements from the DOM
 const mainHeading = document.querySelector('.main-heading');
 const addList = document.getElementById('add-list');
+// An array of tasks
+const tasks = [
+  { description: 'Buy groceries', completed: false, index: 3 },
+  { description: 'Call a friend', completed: false, index: 2 },
+  { description: 'Read a book', completed: true, index: 1 },
+];
