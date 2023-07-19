5829b85b1ee5f5cb954702a2d334eca427bcb377
user-faiz <email2faizm@gmail.com>
2023-07-20 00:48:29 +0530

    update eslint error

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import _ from 'lodash';
 import './style.css';
+
 const tasks = [
-    { description: "Task 1", completed: false, index: 1 },
-    { description: "Task 2", completed: true, index: 2 },
-    { description: "Task 3", completed: false, index: 3 },
-  ];
-  
-  const renderTodoList = () => {
-    const todoListElement = document.getElementById("todo-list");
-  
-    const listItems = tasks.map((task) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = task.description;
-      listItem.classList.add(task.completed ? "completed" : "pending");
-      return listItem;
-    });
-  
-    const todoList = document.createElement("ul");
-    todoList.append(...listItems);
-    todoListElement.appendChild(todoList);
-  };
-  
-  renderTodoList();
-  
+  { description: 'Task 1', completed: false, index: 1 },
+  { description: 'Task 2', completed: true, index: 2 },
+  { description: 'Task 3', completed: false, index: 3 },
+];
+
+const renderTodoList = () => {
+  const todoListElement = document.getElementById('todo-list');
+
+  const listItems = tasks.map((task) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = task.description;
+    listItem.classList.add(task.completed ? 'completed' : 'pending');
+    return listItem;
+  });
+
+  const todoList = document.createElement('ul');
+  todoList.append(...listItems);
+  todoListElement.appendChild(todoList);
+};
+
+renderTodoList();
