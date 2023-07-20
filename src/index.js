69e678f71fdf3048162700d6683ca0773c27ce80
user-faiz <email2faizm@gmail.com>
2023-07-20 14:56:31 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-// Import the CSS and image files
 import './style.css';
 import reloadIcon from './reload.png';
 import moreIcon from './more.png';
-// Select elements from the DOM
 const mainHeading = document.querySelector('.main-heading');
 const addList = document.getElementById('add-list');
-// An array of tasks
+// create array of tasks
 const tasks = [
   { description: 'Buy groceries', completed: false, index: 3 },
   { description: 'Call a friend', completed: false, index: 2 },
@@ -13,9 +11,7 @@ const tasks = [
 ];
 // Function to render the list of tasks
 const render = () => {
-  // Clone the tasks array and sort it based on the 'index' property
   const sortedTasks = tasks.slice().sort((a, b) => a.index - b.index);
-  // Generate HTML list items for each task and join them into a string
   const tasksHTML = sortedTasks.map((task) => `
     <li class='my-each-task'>
       <div class='check-tag'>
@@ -25,10 +21,8 @@ const render = () => {
       <img src=${moreIcon} class='show-more' alt='three dots'/>
     </li>
   `).join('');
-  // Set the HTML content of 'addList' with the generated list items
   addList.innerHTML = tasksHTML;
 };
-// When the DOM content is loaded, call the 'render' function to display the list of tasks
 document.addEventListener('DOMContentLoaded', render);
 // Create and configure the reload image element
 const reloadImg = new Image();
