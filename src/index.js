15f7375416b4716b70066a6ada75d2ec9e1ef726
Faiz Mohammad <134736318+the-faizmohammad@users.noreply.github.com>
2023-07-21 03:55:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-// Import CSS and images
 import './style.css';
 import reloadImage from './reload.png';
 import dltImage from './delete.png';
 import addPlus from './plus.png';
+const appHeading = document.querySelector('.main-heading');
+const todoList = document.getElementById('do-list');
+const addListDiv = document.querySelector('.adding-list');
+const inputTask = document.createElement('input');
+const addButton = document.createElement('button');
+const reloadIcon = new Image();
