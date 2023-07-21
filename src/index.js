4507cd3fd4ed7cc7ed71966cc3b586c331d194c2
Faiz Mohammad <134736318+the-faizmohammad@users.noreply.github.com>
2023-07-21 05:59:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-
+import './style.css';
+import reloadImage from './reload.png';
+import dltImage from './delete.png';
+import addPlus from './plus.png';
+import { updateTaskStatus, clearCompletedTasks } from './statusUpdates.js'; 
